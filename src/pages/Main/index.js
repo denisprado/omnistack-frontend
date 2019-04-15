178f3672649019765fcbcf7e081358178c80cb38
denisforigo <denisforigo@gmail.com>
2019-04-15 00:03:14 -0300

    DELETE route

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -7,6 +7,7 @@ import './styles.scss'
 export default class Main extends Component {
   state = {
     newBox: '',
+    boxes: []
   };
 
   handleSubmit = async e => {
@@ -15,18 +16,42 @@ export default class Main extends Component {
       title: this.state.newBox,
     });
 
-    console.log(response.data)
-
     this.props.history.push(`/box/${response.data._id}`)
+  }
+
+  handleRemove = async (_id) => {
+    const response = await api.delete('boxes', {
+      data: {
+        _id: _id
+      }
+    })
+    const newBoxes = await api.get('boxes');
+    this.setState({ boxes: newBoxes.data })
   }
 
   handleInputChange = (e) => {
     this.setState({ newBox: e.target.value })
   }
 
+  componentDidMount = async () => {
+    const response = await api.get('boxes');
+    this.setState({ boxes: response.data })
+    console.log(this.state.boxes)
+  }
+
   render() {
     return (
       <div id="main-container">
+
+        <ul>
+          {this.state.boxes ? (this.state.boxes.map((box, index) => (
+            <li key={box._id}>
+              <a className="FileInfo" href={"/box/" + box._id}>{box.title}</a><button onClick={() => this.handleRemove(box._id)}>Remove</button>
+            </li>
+          )))
+            : ("Nenhuma box")}
+        </ul>
+
         <form onSubmit={this.handleSubmit}>
           <img src={logo} alt="" />
           <input placeholder="Criar um box" value={this.state.newBox} onChange={this.handleInputChange} />
